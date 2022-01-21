a5a2b713a60993f434698f43ddc7aacc5cfbf13a
chOlya <Chvyrova.o@mail.ru>
2022-01-22 00:03:50 +0300

    Add API key

--- src/api/api.js
+++ src/api/api.js
@@ -4,17 +4,19 @@ const instance = axios.create({
     baseURL: "https://www.googleapis.com/books/v1/volumes",
 })
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 export const bookAPI = {
-    searchBook(searchString, orderBy, category) {
-        if (category != '') {
-            return instance.get(`?maxResults=30&orderBy=${orderBy}&q=${(searchString) + '+subject:' + (category)}`)
+    searchBook(searchString, orderBy, category) { 
+        if (category != ' ') {
+            return instance.get(`?maxResults=30&orderBy=${orderBy}&q=${(searchString) + '+subject:' + (category) + '&' +(apiKey)}`)
         } else {
-            return instance.get(`?maxResults=30&orderBy=${orderBy}&q=${(searchString)}`)
+            return instance.get(`?maxResults=30&orderBy=${orderBy}&q=${(searchString) + '&' +(apiKey)}`)
         }
     },
 
     showBookCard(userId) {
-        return instance.get('/' + userId)
+        return instance.get('/' + userId + '?' + `${apiKey}`)
     },
 
     loadMoreButton(booksCount, searchString) {
